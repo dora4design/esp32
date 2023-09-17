786de1ad6478c6e6a2542e054ab8fd498fb485e4
dora4design <dora4design@gmail.com>
2023-09-17 18:02:08 +0530

    test semaphore on esp32

--- esp32_freertos_semaphore/src/main.cpp
+++ esp32_freertos_semaphore/src/main.cpp
@@ -1,21 +1,37 @@
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "freertos/semphr.h"
 #include <stdio.h>
 
-#define LED_PIN (gpio_num_t)13
+SemaphoreHandle_t xSemaphore = NULL;
+
+TaskHandle_t myTaskHandle1 = NULL;
+TaskHandle_t myTaskHandle2 = NULL;
+
+// function for task1
+void Demo_Task(void *arg) {
+
+  while (1) {
+    printf("Message Sent! [%ld] \n", xTaskGetTickCount());
+    xSemaphoreGive(xSemaphore);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+// functin for task2
+void Demo_Task2(void *arg) {
+
+  while (1) {
+    if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+      printf("Received Message [%ld] \n", xTaskGetTickCount());
+    }
+  }
+}
 
 extern "C" {
 void app_main(void) {
-  esp_rom_gpio_pad_select_gpio(LED_PIN);
-  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-
-  uint8_t ON = 0;
-
-  while (true) {
-    ON = !ON;
-    gpio_set_level(LED_PIN, ON);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+  xSemaphore = xSemaphoreCreateBinary();
+  xTaskCreate(Demo_Task, "Demo_Task", 2098, NULL, 10, &myTaskHandle1);
+  xTaskCreate(Demo_Task2, "Demo_Task2", 2098, NULL, 10, &myTaskHandle2);
 }
 }
